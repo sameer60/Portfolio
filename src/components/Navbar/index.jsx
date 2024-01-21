ef7f5938776430ba5306019e791be6c3a1bc1f50
sameer <shaiksameer6011@gmail.com>
2024-01-21 22:55:22 +0530

    fix navbar responsiveness

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,12 @@
-import "./index.css";
+import "./index.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-left-container">
-        <img
-          className="logo-image"
-          src="https://res.cloudinary.com/didtdrgsj/image/upload/v1689587559/WhatsApp_Image_2023-07-17_at_3.13.56_PM-removebg-preview_etbvpo.png"
-          alt="logo"
-        />
-        {/* <a href="/" className="link">
+        <a href="/" className="logo-name">
           Sameer
-        </a> */}
+        </a>
       </div>
       <div className="navbar-links-container">
         <a className="link" href="/about">
